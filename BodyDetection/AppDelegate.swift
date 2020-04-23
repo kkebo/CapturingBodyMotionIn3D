e323a32263ed8f80a4bed00bb361d2ca683e2ea5
Kenta Kubo <kkk669@users.noreply.github.com>
2020-04-24 01:01:33 +0900

    Refactor

--- BodyDetection/AppDelegate.swift
+++ BodyDetection/AppDelegate.swift
@@ -9,13 +9,15 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
-        -> Bool {
-        return true
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    )
+        -> Bool
+    {
+        true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -32,5 +34,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
     }
-
 }
--- BodyDetection/ViewController.swift
+++ BodyDetection/ViewController.swift
@@ -5,24 +5,24 @@ Abstract:
 The sample app's main view controller.
 */
 
-import UIKit
-import RealityKit
 import ARKit
 import Combine
+import RealityKit
+import UIKit
 
 class ViewController: UIViewController, ARSessionDelegate {
-
     @IBOutlet var arView: ARView!
-    
+
     // The 3D character to display.
-    var character: BodyTrackedEntity?
-    let characterOffset: SIMD3<Float> = [-1.0, 0, 0] // Offset the character by one meter to the left
+    let characterOffset: SIMD3<Float> = [0, 0, 0]  // Offset the character by one meter to the left
     let characterAnchor = AnchorEntity()
-    
+
+    var cancellables = Set<AnyCancellable>()
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         arView.session.delegate = self
-        
+
         // If the iOS device doesn't support body tracking, raise a developer error for
         // this unhandled case.
         guard ARBodyTrackingConfiguration.isSupported else {
@@ -32,45 +32,33 @@ class ViewController: UIViewController, ARSessionDelegate {
         // Run a body tracking configration.
         let configuration = ARBodyTrackingConfiguration()
         arView.session.run(configuration)
-        
+
         arView.scene.addAnchor(characterAnchor)
-        
+
+        self.characterAnchor.isEnabled = false
+
         // Asynchronously load the 3D character.
-        var cancellable: AnyCancellable? = nil
-        cancellable = Entity.loadBodyTrackedAsync(named: "character/robot").sink(
-            receiveCompletion: { completion in
-                if case let .failure(error) = completion {
-                    print("Error: Unable to load model: \(error.localizedDescription)")
-                }
-                cancellable?.cancel()
-        }, receiveValue: { (character: Entity) in
-            if let character = character as? BodyTrackedEntity {
+        Entity.loadBodyTrackedAsync(named: "character/robot")
+            .mapError { fatalError("Error: Unable to load model: \($0.localizedDescription)") }
+            .sink {
                 // Scale the character to human size
-                character.scale = [1.0, 1.0, 1.0]
-                self.character = character
-                cancellable?.cancel()
-            } else {
-                print("Error: Unable to load model as BodyTrackedEntity")
+                $0.scale = [1.0, 1.0, 1.0]
+                self.characterAnchor.addChild($0)
             }
-        })
+            .store(in: &self.cancellables)
     }
-    
+
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
-        for anchor in anchors {
-            guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
-            
+        for anchor in anchors.compactMap({ $0 as? ARBodyAnchor }) {
             // Update the position of the character anchor's position.
-            let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
+            let bodyPosition = simd_make_float3(anchor.transform.columns.3)
             characterAnchor.position = bodyPosition + characterOffset
             // Also copy over the rotation of the body anchor, because the skeleton's pose
             // in the world is relative to the body anchor's rotation.
-            characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
-   
-            if let character = character, character.parent == nil {
-                // Attach the character to its anchor as soon as
-                // 1. the body anchor was detected and
-                // 2. the character was loaded.
-                characterAnchor.addChild(character)
+            characterAnchor.orientation = Transform(matrix: anchor.transform).rotation
+
+            if !characterAnchor.isEnabled {
+                characterAnchor.isEnabled = true
             }
         }
     }
